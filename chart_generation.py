9071c4483020c99100456a1c89062d890e941382
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-03-31 19:34:08 -0500

    added graph generation (wip)

--- chart_generation.py
+++ chart_generation.py
@@ -1,1 +1,26 @@
+import pygal
+
+
 ##Generate a graph and open in the user’s default browser.
+## bar graph
+def generate_chart(labels, values, chart_type='1'):
+    
+    if chart_type == '1':
+        bar_chart = pygal.Bar()
+        bar_chart.title = "stock data bar graph"
+        bar_chart.x_labels = labels
+        bar_chart.add("Values", values)
+        bar_chart.render()
+    
+    if chart_type == '2':
+        line_chart = pygal.Line()
+        line_chart.title = "stock data line graph"
+        line_chart.x_labels = labels
+        line_chart.add("Values", values)
+        line_chart.render()
+
+
+
+
+
+
