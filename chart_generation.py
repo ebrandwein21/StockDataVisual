dbd44e09477e06d410c27f4558e9ef1289e18969
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-04-01 16:39:32 -0500

    added error checking

--- chart_generation.py
+++ chart_generation.py
@@ -1,38 +1,29 @@
 import pygal
 
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 366a310ddee9e85ef1fd964775b93c78ab645d71
 ##Generate a graph and open in the user’s default browser.
 ## bar graph
-def generate_chart(labels,values, chart_type, symbol):
-
-
+def generate_chart(filtered_data, chart_type='1', symbol=''):
+    
+    dates = list(filtered_data.keys())
+    values = [float(data['Prices']) for data in filtered_data.values()]
+    
     if chart_type == '1':
         bar_chart = pygal.Bar()
-        bar_chart.title = f"stock data bar graph"
-        bar_chart.x_labels = labels
-        bar_chart.add("Values", values)
+        bar_chart.title = f"{symbol}stock data bar graph"
+        bar_chart.x_labels = dates
+        bar_chart.add("Price", values)
         bar_chart.render_in_browser()
     
     if chart_type == '2':
         line_chart = pygal.Line()
-        line_chart.title = f"stock data line graph"
-        line_chart.x_labels = labels
-        line_chart.add("Values", values)
+        line_chart.title = f"{symbol}stock data line graph"
+        line_chart.x_labels = dates
+        line_chart.add("Price", values)
         line_chart.render_in_browser()
 
 
 
-def test_chart():
-    line_chart = pygal.Line()
-    line_chart.title = 'Browser usage evolution (in %)'
-    line_chart.x_labels = map(str, range(2002, 2013))
-    line_chart.add('Firefox', [None, None,    0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
-    line_chart.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
-    line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
-    line_chart.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
-    line_chart.render_in_browser()
+
+
 
